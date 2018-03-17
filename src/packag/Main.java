fe1cbdd281f8509cd7b8c44bf4e3e236f4aea444
Nestor <Nestor@DESKTOP-NLIP3DV>
2018-03-17 13:00:07 +0100

    modif

--- src/packag/Main.java
+++ src/packag/Main.java
@@ -3,7 +3,7 @@ package packag;
 public class Main {
 
 	public static void main(String[] args) {
-		TestGIT myFIrtsTestGit = new TestGitUpdate("Loooolll");
+		TestGIT myFIrtsTestGit = new TestGitUpdate("Owner parameter");
 		System.out.println(myFIrtsTestGit.toString());
 	}
 
