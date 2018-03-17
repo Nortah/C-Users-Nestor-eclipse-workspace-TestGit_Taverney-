f35894cb40e40fc101dd3ea66017067850b33de6
Nestor <Nestor@DESKTOP-NLIP3DV>
2018-03-17 12:19:09 +0100

    Main

--- src/packag/TestGIT.java
+++ src/packag/TestGIT.java
@@ -11,5 +11,11 @@ public class TestGIT {
 	public String toString(){
 		return this.gitFromScratch;
 	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+		
+	}
 
 }
